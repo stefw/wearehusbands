1d11198f3913f3cced7103f2ad79953455ea8996
stefw <sw@magicmorning.net>
2014-11-18 23:32:50 +0100

    initial import.

---
+++ index.php
@@ -0,0 +1,132 @@
+<!doctype html>
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+	<title>Husbands</title>
+	<meta name="description" content="We are Husbands">
+		
+	<meta name="apple-mobile-web-app-capable" content="yes" />
+	<meta name="apple-mobile-web-app-title" content="Check-up Conseil">
+	<meta name="mobile-web-app-capable" content="yes">
+	<meta name="HandheldFriendly" content="True">
+    <meta name="MobileOptimized" content="375">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, minimal-ui" />
+    <link type="text/plain" rel="author" href="/humans.txt" />
+    
+    <link rel="apple-touch-icon" sizes="57x57" href="/favicons/apple-touch-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="/favicons/apple-touch-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="/favicons/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="/favicons/apple-touch-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="/favicons/apple-touch-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="/favicons/apple-touch-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="/favicons/apple-touch-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="/favicons/apple-touch-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon-180x180.png">
+	<link rel="icon" type="image/png" href="/favicons/favicon-192x192.png" sizes="192x192">
+	<link rel="icon" type="image/png" href="/favicons/favicon-160x160.png" sizes="160x160">
+	<link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96">
+	<link rel="icon" type="image/png" href="/favicons/favicon-16x16.png" sizes="16x16">
+	<link rel="icon" type="image/png" href="/favicons/favicon-32x32.png" sizes="32x32">
+	<meta name="msapplication-TileColor" content="#000000">
+	<meta name="msapplication-TileImage" content="/favicons/mstile-144x144.png">
+
+	<link rel="stylesheet" href="magnific-popup.css"> 
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script> 
+	<script src="jquery.magnific-popup.js"></script> 
+	<link rel="stylesheet" href="main.css"> 
+	
+	<meta property="og:locale" content="fr_FR" />
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content="We are Husbands" />
+	<meta property="og:description" content="LET ME DOWN (DON'T) EP" />
+	<meta property="og:url" content="http://www.wearehusbands.com/" />
+	<meta property="og:site_name" content="wearehusbands" />
+	<meta property="fb:admins" content="553093426" />
+	<meta property="og:image" content="http://www.wearehusbands.com/700.png" />
+
+</head>
+
+<body>
+	
+<div class="container popup-vimeo">
+
+	<section id="txt">
+		
+		<div id="logo">
+			<img src="logo.png" alt="Husbands" title="Husbands" />
+		</div>
+			
+		<div id="sortie">
+			<h2>LET ME DOWN (DON'T) EP</h2>
+			<ul>
+				<li>Release December 2014 on Sounds Like Yeah!</li>
+				<li>Including remixes by Laurent Garnier & Ghost of Christmas</li>
+				<li>Stay tuned! </li>
+			</ul>
+			
+			<p class="social">
+				Follow us &mdash; <a href="http://facebook.com/wearehusbands" class="link">Facebook</a> - <a href="http://www.twitter.com/wearehusbands"  class="link">Twitter</a> - <a href="http://www.soundcloud.com/wearehusbands"  class="link">Soundcloud</a> - <a href="http://www.instagram.com/wearehusbands"  class="link">Instagram</a> - <a href="http://www.youtube.com/husbandsmarseille"  class="link">Youtube</a> - <a href="http://wearehusbands.bandcamp.com"  class="link">Bandcamp</a>
+			</p>
+			<p class="social sly">
+			Contacts &mdash; <a href="mailto:wearehusbands@gmail.com" class="link">husbands</a> -  <a href="mailto:contact@soundslikeyeah.com" class="link">sly records </a> -  <a href="mailto:jb@bleucitron.net" class="link">booking</a> -  <a href="mailto:olivier@alter-k.com" class="link">publishing</a> -  <a href="mailto:virginie@soundslikeyeah.com" class="link">promo</a>
+			</p>		
+			
+		</div>
+	
+	</section>
+
+</div>
+
+
+
+<script type="text/javascript">
+  	$(document).ready(function() {
+		    $('.popup-vimeo').magnificPopup({
+		         items: {
+			        src: 'http://vimeo.com/112037829',
+			  },
+		
+		      disableOn: 900,
+		      type: 'iframe',
+		      mainClass: 'mfp-fade',
+		      removalDelay: 160,
+		      preloader: false,
+		      fixedContentPos: false,
+		      
+		      disableOn: function() {
+				  if($('.link').data('clicked') == true) {
+				    return false;
+				  } 
+				  return true;
+				}
+
+		    });
+		    
+			$( "a.link" ).click(function() {
+			 	window.location.href = this.getAttribute('href');
+			 	$('.link').data('clicked', true);
+
+			});
+		    
+
+    });  
+</script>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-56850872-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+</body>
+</html>
